e80df9ce7f45cd5bf381320f8e3b774511390c3a
teemulaiho <60133204+teemulaiho@users.noreply.github.com>
2021-10-13 10:18:21 +0200

    Added HealthBars Prefabs to Enemy Prefab
    HealthbarBehaviour.cs gets the Health -component of its parent GameObject, and listens to SetHealthAction -calls. The event is notified when the enemy takes damage.

---
+++ Roguelike TD/Assets/Scripts/HealthbarBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthbarBehaviour : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] Gradient gradient;
+    [SerializeField] Image fill;
+
+    private Health healthScript;
+
+    /*
+     * When adding a HealthBar to a GameObject, remember to add the following code to the GameObjects Awake -function:
+     * 
+     * healthBar = GetComponentInChildren<HealthbarBehaviour>();
+     * if (healthBar != null)
+     * {
+     *      healthBar.SetMaxHealth(baseMaxHealth);
+     * }  
+     * 
+     * OR
+     * 
+     * Add the GameObject or Health Reference to the HealthBar instead.
+     * 
+     * GameObject parent = GetComponentInParent<GameObject>();
+     * 
+     *         health = GetComponentInParent<Health>();
+     *         if (health != null)
+     *         {
+     *              SetMaxHealth(health.GetMaxHealth());
+     *         }
+    */
+
+    private void Start()
+    {
+        healthScript = GetComponentInParent<Health>();
+
+        if (healthScript != null)
+        {
+            SetMaxHealth(healthScript.GetMaxHealth());
+            healthScript.SetHealth += SetHealth;
+        }
+    }
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
